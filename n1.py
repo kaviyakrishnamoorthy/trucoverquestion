5e30c15c1c366a93b25fa40976a5f59542d66ad9
kaviyakrishnamoorthy <45667648+kaviyakrishnamoorthy@users.noreply.github.com>
2019-04-01 18:20:38 +0530

    Add files via upload

---
+++ n1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+d=pd.read_csv('F:\\truecover\\ml-20m-youtube\\ml-youtube.csv')
+#print(d)
+for i in range(1,len(d)):
+    if d["title"][i]==video_id:
+        print(d["youtubeId"][i]
+    
+
